ca1539dec4063d7a9940c55d5fc5177711314a59
dvlproad <studyroad@qq.com>
2017-11-11 06:26:30 +0800

    【Feature】增加图片选择要用到的保存数据方法（之前被整理到CJDataDiskManager了）

---
+++ CJFile/CJFileManager/CJFileManager+SaveFileData.h
@@ -0,0 +1,27 @@
+//
+//  CJFileManager+SaveFileData.h
+//  CommonFMDBUtilDemo
+//
+//  Created by ciyouzen on 6/25/15.
+//  Copyright (c) 2015 dvlproad. All rights reserved.
+//
+
+#import "CJFileManager.h"
+
+///数据保存(图片选择后需要用)
+@interface CJFileManager (SaveData)
+
+/**
+ *  保存文件到以home相对的相对路径下
+ *
+ *  @param data                     文件数据
+ *  @param fileName                 文件以什么名字保存
+ *  @param relativeDirectoryPath    文件保存的相对路径
+ *
+ *  return 是否保存成功
+ */
++ (BOOL)saveFileData:(NSData *)data
+        withFileName:(NSString *)fileName
+toRelativeDirectoryPath:(NSString *)relativeDirectoryPath;
+
+@end
